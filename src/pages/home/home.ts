432ec0dc6c69d7432f3726c42ad0d1ebae1fea85
parallel-kevin <601406483@qq.com>
2017-05-05 20:58:08 +0800

    文案审核通过的样式

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,21 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   location: string;
+  weibo: string;
   submitted: boolean;
 
   constructor(public navCtrl: NavController) {
     this.location = '';
     this.submitted = false;
-
+    this.weibo = '';
   }
 
   searchLoc() {
+    /*var request = new XMLHttpRequest();
+    request.open("GET","http://120.25.238.161:8080/servlet/Weather?city="+this.location,false);
+    request.send();
+    this.weibo = request.responseText;*/
     this.submitted = true;
     this.location = '';
   }
 
   sendWeibo() {
-    
+
   }
 
   clear() {
